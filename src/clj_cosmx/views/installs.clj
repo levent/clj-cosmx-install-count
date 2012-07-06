4daefb9d9e81c06a219c4fe127142d5ad8deffc7
Levent Ali <lebreeze@gmail.com>
2012-07-06 13:42:47 +0100

    Minimal code

--- src/clj_cosmx/views/installs.clj
+++ src/clj_cosmx/views/installs.clj
@@ -1,5 +1,4 @@
 (ns clj-cosmx.views.installs
-  (:require [clj-cosmx.views.common :as common])
   (:require [clj-http.client :as client])
   (:require [cheshire.core :as json])
             
@@ -7,8 +6,9 @@
         [hiccup.core :only [html]]))
 
 (defn get-cosmx-install-count []
-  (get (json/parse-string (get (client/get (str "http://api.cosm.com/v2/feeds.json?tag=app%3Aname%3DCosmX&content=summary&key=" (get (System/getenv) "API_KEY")) ) :body) true) :totalResults))
+  (get (json/parse-string
+         (get (client/get
+                (str "http://api.cosm.com/v2/feeds.json?tag=app%3Aname%3DCosmX&content=summary&key=" (get (System/getenv) "API_KEY")) ) :body) true) :totalResults))
 
 (defpage "/" []
-         (common/layout
-           [:p (get-cosmx-install-count)]))
+         (str ( get-cosmx-install-count)))
